747e06b23789a5404786cb953816ebceb8ed5baa
GoldenAnpu <seryios.sych@gmail.com>
2023-04-20 10:54:31 +0200

    update def; expand example

--- src/main.py
+++ src/main.py
@@ -10,18 +10,16 @@ api = sly.Api.from_env()
 project_id = sly.env.project_id()
 dataset_id = sly.env.dataset_id()
 
-vide_ids = api.video.get_list(dataset_id)
+video_ids = api.video.get_list(dataset_id)
 
 project_meta_json = api.project.get_meta(project_id)
 project_meta = sly.ProjectMeta.from_json(data=project_meta_json)
 
-key_id_map = sly.KeyIdMap()
-
-video_ann_json = api.video.annotation.download(vide_ids[0].id)
+video_ann_json = api.video.annotation.download(video_ids[0].id)
 
 
-def refresh_meta(project_meta, new_tag_meta, tag_name):
-    if not project_meta.tag_metas.has_key(tag_name):
+def refresh_meta(project_meta, new_tag_meta):
+    if not project_meta.tag_metas.has_key(new_tag_meta.name):
         new_tags_collection = project_meta.tag_metas.add(new_tag_meta)
         project_meta = sly.ProjectMeta(
             tag_metas=new_tags_collection, obj_classes=project_meta.obj_classes
@@ -32,7 +30,7 @@ def refresh_meta(project_meta, new_tag_meta, tag_name):
         new_tag_meta = project_meta.tag_metas.get(new_tag_meta.name)
     else:
         tag_values = new_tag_meta.possible_values
-        new_tag_meta = project_meta.tag_metas.get(tag_name)
+        new_tag_meta = project_meta.tag_metas.get(new_tag_meta.name)
         if tag_values:
             if sorted(new_tag_meta.possible_values) != sorted(tag_values):
                 sly.logger.warning(
@@ -49,11 +47,11 @@ video_tag_meta = sly.TagMeta(
     applicable_to=sly.TagApplicableTo.ALL,
 )
 
-new_tag_meta, project_meta = refresh_meta(project_meta, video_tag_meta, video_tag_meta.name)
+new_tag_meta, project_meta = refresh_meta(project_meta, video_tag_meta)
 
-api.video.tag.add_tag(new_tag_meta.sly_id, vide_ids[0].id, value=3)
+api.video.tag.add_tag(new_tag_meta.sly_id, video_ids[0].id, value=3)
 
-tag_info = api.video.tag.add_tag(new_tag_meta.sly_id, vide_ids[0].id, value=2, frame_range=[2, 6])
+tag_info = api.video.tag.add_tag(new_tag_meta.sly_id, video_ids[0].id, value=2, frame_range=[2, 6])
 
 api.video.tag.update_value(tag_id=tag_info["id"], tag_value=1)
 
@@ -62,16 +60,24 @@ api.video.tag.update_frame_range(tag_info["id"], [3, 5])
 api.video.tag.remove_from_video(tag_info["id"])
 
 
-########## annotated objects ########
+########## annotated objects ##########
 
-object_tag_meta = sly.TagMeta(
+orange_object_tag_meta = sly.TagMeta(
     name="orange",
     value_type=sly.TagValueType.ONEOF_STRING,
     applicable_to=sly.TagApplicableTo.OBJECTS_ONLY,
     possible_values=["small", "big"],
 )
 
-new_tag_meta, _ = refresh_meta(project_meta, object_tag_meta, object_tag_meta.name)
+kiwi_object_tag_meta = sly.TagMeta(
+    name="kiwi",
+    value_type=sly.TagValueType.ONEOF_STRING,
+    applicable_to=sly.TagApplicableTo.OBJECTS_ONLY,
+    possible_values=["big", "small"],
+)
+
+orange_new_tag_meta, project_meta = refresh_meta(project_meta, orange_object_tag_meta)
+kiwi_new_tag_meta, _ = refresh_meta(project_meta, kiwi_object_tag_meta)
 
 project_objects = video_ann_json.get("objects")
 
@@ -79,10 +85,11 @@ created_tag_ids = {}
 for object in project_objects:
     if object["classTitle"] == "orange":
         tag_id = api.video.object.tag.add(
-            new_tag_meta.sly_id, object["id"], value="big", frame_range=[2, 6]
+            orange_new_tag_meta.sly_id, object["id"], value="big", frame_range=[2, 6]
         )
-        api.video.object.tag.add(new_tag_meta.sly_id, object["id"], value="small")
         created_tag_ids[object["id"]] = tag_id
+    elif object["classTitle"] == "kiwi":
+        api.video.object.tag.add(kiwi_new_tag_meta.sly_id, object["id"], value="big")
 
 orange_ids = [object["id"] for object in project_objects if object["classTitle"] == "orange"]
 
